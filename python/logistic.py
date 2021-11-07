100f203f4331d395443d4d4128d0b4473f3555a6
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2021-11-07 09:45:50 -0500

    Implementing official ovr approach

--- python/logistic.py
+++ python/logistic.py
@@ -10,7 +10,7 @@ if __name__=='__main__':
   
   '''with open('test.pkl','rb') as f:
     data = pickle.load(f)'''
-  points = 3
+  points = 15
   data = getData(points)
   x = []
   y = []
@@ -23,15 +23,13 @@ if __name__=='__main__':
     y.append(part[1])
   scaler = StandardScaler()
   x = np.array(x)
-  temp = list(zip(x, y))
-  random.shuffle(temp)
-  x, y = zip(*temp)
-  x_train, x_test = np.split(x, [int(len(x)*0.8)])
-  for i in range(points):
-    yVsAll = []
-    for yPart in y:
-      yVsAll.append(0 if yPart == i+1 else 1)
-    y_train, y_test = np.split(yVsAll, [int(len(x)*0.8)])
-    clf = LogisticRegression(random_state=0, max_iter=1000000).fit(x_train, y_train)
+  for i in range(10):
+    temp = list(zip(x, y))
+    random.seed(i)
+    random.shuffle(temp)
+    x, y = zip(*temp)
+    x_train, x_test = np.split(x, [int(len(x)*0.8)])
+    y_train, y_test = np.split(y, [int(len(x)*0.8)])
+    clf = LogisticRegression(random_state=0, max_iter=10000000, multi_class='ovr').fit(x_train, y_train)
     print(clf.predict(x_test))
     print(clf.score(x_test, y_test))
